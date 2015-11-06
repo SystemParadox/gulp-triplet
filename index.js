20ee5ec96d88957bc52b2139ca408152225a4d78
Simon Williams <simon@systemparadox.co.uk>
2015-11-06 23:47:59 +0000

    Use filename from vinyl stream when available

--- index.js
+++ index.js
@@ -4,10 +4,13 @@ var through = require('through2');
 var PluginError = gutil.PluginError;
 var _ = require('underscore');
 var triplet = require('triplet');
+var path = require('path');
 
 
 module.exports = function (options) {
     return through.obj(function(file, enc, cb) {
+        options = _.extend({}, options);
+        options.filename = path.relative(file.cwd, file.path);
         if (file.isBuffer()) {
             var output = triplet(file.contents, options);
             file.contents = new Buffer(output);
