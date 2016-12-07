0372556927b20b9163312a2419e2380fd2f3dbc4
Simon Williams <simon@systemparadox.co.uk>
2016-12-07 08:31:50 +0000

    Emit error instead of throwing

--- index.js
+++ index.js
@@ -1,28 +1,44 @@
 'use strict';
+var path = require('path');
 var gutil = require('gulp-util');
 var through = require('through2');
-var PluginError = gutil.PluginError;
-var _ = require('underscore');
+var objectAssign = require('object-assign');
 var triplet = require('triplet');
-var path = require('path');
 
 
-module.exports = function (options) {
-    return through.obj(function(file, enc, cb) {
-        var opts = _.extend({}, options);
-        if (file.path && ! opts.filename) {
-            opts.filename = path.relative(file.cwd, file.path);
-        }
-        if (file.isBuffer()) {
-            var output = triplet(file.contents, opts);
-            file.contents = new Buffer(output);
+module.exports = function (opts) {
+    opts = opts || {};
+
+    return through.obj(function (file, enc, cb) {
+        if (file.isNull()) {
+            cb(null, file);
+            return;
         }
 
-        if (file.isStream()) {
-            file.contents = triplet(file.contents, opts);
+        try {
+            var fileOpts = objectAssign({}, opts);
+
+            if (file.path && ! opts.filename) {
+                fileOpts.filename = path.relative(file.cwd, file.path);
+            }
+
+            if (file.isBuffer()) {
+                var output = triplet(file.contents, fileOpts);
+                file.contents = new Buffer(output);
+            }
+
+            if (file.isStream()) {
+                file.contents = triplet(file.contents, opts);
+            }
+
+            this.push(file);
+        } catch (err) {
+            this.emit('error', new gutil.PluginError('gulp-triplet', err, {
+                fileName: file.path,
+                showProperties: false
+            }));
         }
 
-        this.push(file);
         cb();
     });
 };
